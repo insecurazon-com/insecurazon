4c11181552c5c1d8a2aec4daf0de815d9253962e
Levente Simon <ls@leventesimon.com>
2025-05-12 21:45:55 +0200

    terraform config files for init

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "s3" {
+    bucket         = "insecurazon-terraform-state-bucket"
+    key            = "path/to/your/terraform.tfstate"
+    region         = "eu-central-1"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
